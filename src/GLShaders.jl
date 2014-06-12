d1d08e2fccdf2e1b46b205e5291aa4a3e40156c6
Simon Danisch <sdanisch@gmail.com>
2014-06-13 00:32:34 +0200

    first step in direction of good shader reading

--- src/GLShaders.jl
+++ src/GLShaders.jl
@@ -1,19 +1,6 @@
-using GLFW, React, ImmutableArrays, ModernGL, GLUtil
-
-function createWindow(name::Symbol, w, h)
-	GLFW.WindowHint(GLFW.SAMPLES, 4)
-	#GLFW.WindowHint(GLFW.CONTEXT_VERSION_MAJOR, 3)
-	#GLFW.WindowHint(GLFW.CONTEXT_VERSION_MINOR, 3)
-	#GLFW.WindowHint(GLFW.OPENGL_FORWARD_COMPAT, GL_TRUE)
-	#GLFW.WindowHint(GLFW.OPENGL_PROFILE, GLFW.OPENGL_CORE_PROFILE)
-
-	window = GLFW.CreateWindow(w, h, string(name))
-	GLFW.MakeContextCurrent(window)
-end
+using GLFW, React, ImmutableArrays, ModernGL, GLUtil, GLWindow
 
 
-
-GLFW.Init()
 
 
 createWindow(:lol, 10, 10)
@@ -24,21 +11,25 @@ immutable GLSLVariable
 	location::GLint
 	typ::GLenum
 end
-flatshader = GLProgram("/home/s/.julia/v0.3/GLWindow/src/flatShader")
-uniformLength = glGetProgramiv(flatshader.id, GL_ACTIVE_UNIFORMS)
+
+
+flatshader 		= GLProgram("/home/s/.julia/v0.3/GLPlot/src/volumeShader")
+uniformLength 	= glGetProgramiv(flatshader.id, GL_ACTIVE_UNIFORMS)
 attributeLength = glGetProgramiv(flatshader.id, GL_ACTIVE_ATTRIBUTES)
-uniforms = Array(GLUniform, uniformLength)
-attributes = Array(GLUniform, attributeLength)
 
-glGetActiveAttrib(programID::GLuint, index::Integer) = GLSLVariable(glGetActiveAttrib(programID, index)...)
-glGetActiveUniform(programID::GLuint, index::Integer) = GLSLVariable(glGetActiveAttrib(programID, index)...)
+uniforms 		= Array(GLSLVariable, uniformLength)
 
-for i::GLint=0:uniformLength-1
-	uniforms[i+1] = glGetActiveAttrib(flatshader.id, i)
-	println(uniforms[i+1])
+if uniformLength > 0
+	for i::GLint=0:uniformLength-1
+		uniforms[i+1] = GLSLVariable(glGetActiveUniform(flatshader.id, i)...)
+		println(uniforms[i+1])
+	end
 end
-
-for i::GLint=0:attributeLength-1
-	attributes[i+1] = glGetActiveAttrib(flatshader.id, i)
-	println(uniforms[i+1])
+println(attributeLength)
+if attributeLength > 0
+	attributes 		= Array(GLSLVariable, attributeLength)
+	for i::GLint=0:attributeLength-1
+		attributes[i+1] = GLSLVariable(glGetActiveAttrib(flatshader.id, i)...)
+		println(attributes[i+1])
+	end
 end
