0f9472038f426f4996731d4587cc7965abef554f
Simon Danisch <sdanisch@gmail.com>
2014-06-17 00:54:35 +0200

    started integrating Immutable Arrays... not tested

--- src/GLMatrixMath.jl
+++ src/GLMatrixMath.jl
@@ -1,42 +1,40 @@
-computeOrthographicProjection(
-                        left::GLfloat,   right::GLfloat,
-                        bottom::GLfloat, top::GLfloat,
-                        znear::GLfloat,  zfar::GLfloat) = computeOrthographicProjection!(zeros(GLfloat, 4, 4), left, right, bottom, top, znear,  zfar)
+using ImmutableArrays
 
-function computeOrthographicProjection!(
-                        matrix::Matrix,
-                        left::GLfloat,   right::GLfloat,
-                       bottom::GLfloat, top::GLfloat,
-                       znear::GLfloat,  zfar::GLfloat)
+
+function orthographicprojection{T}(
+                        left::T,   right::T,
+                       bottom::T, top::T,
+                       znear::T,  zfar::T)
 
     @assert right  != left 
     @assert bottom != top  
     @assert znear  != zfar 
 
-    fill!(matrix, 0f0)
+    matrix = zeros(T, 4,4)
 
-    matrix[1,1] = 2.0/(right-left);
-    matrix[1,4] = -(right+left)/(right-left);
-    matrix[2,2] = 2.0/(top-bottom);
-    matrix[2,4] = -(top+bottom)/(top-bottom);
-    matrix[3,3] = -2.0/(zfar-znear);
-    matrix[3,4] = -(zfar+znear)/(zfar-znear);
-    matrix[4,4] = 1.0;
-    matrix
+    matrix[1,1] = 2.0/(right-left)
+    matrix[1,4] = -(right+left)/(right-left)
+    matrix[2,2] = 2.0/(top-bottom)
+    matrix[2,4] = -(top+bottom)/(top-bottom)
+    matrix[3,3] = -2.0/(zfar-znear)
+    matrix[3,4] = -(zfar+znear)/(zfar-znear)
+    matrix[4,4] = 1.0
+    Matrix4x4{T}(matrix)
 end
 
-translateXMatrix(x::GLfloat) = translationMatrix( x, 0.0f0, 0.0f0)
-translateYMatrix(y::GLfloat) = translationMatrix( 0.0f0, y, 0.0f0)
-translateZMatrix(z::GLfloat) = translationMatrix( 0.0f0, 0.0f0, z)
+translateXMatrix{T}(x::T) = translationMatrix( Vector3{T}(x, 0, 0))
+translateYMatrix{T}(y::T) = translationMatrix( Vector3{T}(0, y, 0))
+translateZMatrix{T}(z::T) = translationMatrix( Vector3{T}(0, 0, z))
 
-function translationMatrix(translation::Array{GLfloat,1})
-    result      = eye(GLfloat, 4, 4)
-    result[1:3,4] = translation
-    return result
+function translationMatrix{T}(translation::Vector3{T})
+    result          = eye(T, 4, 4)
+    result[1:3,4]   = translation
+
+    return Matrix4x4{T}(result)
 end
 
 
-function frustum(left, right, bottom, top, znear, zfar):
+function frustum{T}(left::T, right::T, bottom::T, top::T, znear::T, zfar::T)
     # Create view frustum
 
     # Parameters
@@ -63,7 +61,7 @@ function frustum(left, right, bottom, top, znear, zfar):
     @assert bottom != top
     @assert znear != zfar
 
-    M = zeros(Float32, 4, 4)
+    M = zeros(T, 4, 4)
     M[1, 1] = +2.0 * znear / (right - left)
     M[3, 1] = (right + left) / (right - left)
     M[2, 2] = +2.0 * znear / (top - bottom)
@@ -71,10 +69,10 @@ function frustum(left, right, bottom, top, znear, zfar):
     M[3, 3] = -(zfar + znear) / (zfar - znear)
     M[4, 3] = -2.0 * znear * zfar / (zfar - znear)
     M[3, 4] = -1.0
-    return M
+    return Matrix4x4{T}(M)
 end
 
-function pProj(fovy, aspect, znear, zfar):
+function perspectiveprojection{T}(fovy::T, aspect::T, znear::T, zfar::T)
     # Create perspective projection matrix
 
     # Parameters
@@ -96,9 +94,9 @@ function pProj(fovy, aspect, znear, zfar):
     @assert znear != zfar
     h = tan(fovy / 360.0 * pi) * znear
     w = h * aspect
-    return frustum(-w, w, -h, h, znear, zfar)
+    return frustum{T}(-w, w, -h, h, znear, zfar)
 end
-function lookAt(eyePos::Array{Float32, 1}, lookAt::Array{Float32, 1}, up::Array{Float32, 1}) 
+function lookat{T}(eyePos::Vector3{T}, lookAt::Vector3{T}, up::Vector3{T}) 
  
     zaxis  = eyePos - lookAt
     zaxis  *= 1.0 / norm(zaxis)
@@ -111,54 +109,41 @@ function lookAt(eyePos::Array{Float32, 1}, lookAt::Array{Float32, 1}, up::Array{
     viewMatrix[2,1:3]  = yaxis
     viewMatrix[3,1:3]  = zaxis
 
-    viewMatrix * translationMatrix(-eyePos)
+    Matrix4x4(viewMatrix) * translationMatrix(-eyePos)
 end
-function rotatationMatrixX(angle::Float32)
-    Float32[1 0 0 0;
-     0 cos(angle) -sin(angle) 0;
-     0 sin(angle) cos(angle) 0;
-     0 0 0 1]
+function rotatationMatrixX{T}(angle::T)
+    Matrix4x4{T}(
+        Vector4{T}(1, 0, 0, 0),
+        Vector4{T}(0, cos(angle), -sin(angle), 0),
+        Vector4{T}(0, sin(angle), cos(angle), 0),
+        Vector4{T}(0, 0, 0, 1))
 end
-function rotatationMatrixY(angle::Float32)
-    Float32[cos(angle) 0 sin(angle) 0;
-     0 1 0 0;
-     -sin(angle) 0 cos(angle) 0;
-     0 0 0 1]
+function rotatationMatrixY{T}(angle::T)
+    Matrix4x4{T}(
+        Vector4{T}(cos(angle), 0, sin(angle), 0),
+        Vector4{T}(0, 1, 0, 0),
+        Vector4{T}(-sin(angle), 0, cos(angle), 0),
+        Vector4{T}(0, 0, 0, 1))
 end
-function rotatationMatrixZ(angle::Float32)
-    Float32[cos(angle) -sin(angle) 0 0;
-     sin(angle) cos(angle) 0  0;
-     0 0 1 0;
-     0 0 0 1]
+function rotatationMatrixZ{T}(angle::T)
+    Matrix4x4{T}(
+        Vector4{T}(cos(angle), -sin(angle), 0, 0),
+        Vector4{T}(sin(angle), cos(angle), 0,  0),
+        Vector4{T}(0, 0, 1, 0),
+        Vector4{T}(0, 0, 0, 1))
 end
-function rotatationMatrix(angle::Float32, axis)
+function rotatationMatrix{T}(angle::T, axis::Vector3{T})
     x = axis[1]
     y = axis[2]
     z = axis[3]
     a = angle
-    m1 = Float32[ x^2 * (1 - cos(a)) + cos(a)  ,  x*y * (1 - cos(a)) - z*sin(a),  x*z * (1 - cos(a)) + y*sin(a), 0]
-    m2 = Float32[ x*y * (1 - cos(a)) + z*sin(a),  y^2 * (1 - cos(a)) + cos(a)  ,  y*z * (1 - cos(a)) - x*sin(a), 0]
-    m3 = Float32[ x*z * (1 - cos(a)) - y*sin(a),  y*z * (1 - cos(a)) + x*sin(a),  z^2 * (1 - cos(a)) + cos(a)  , 0]
-    
-    [m1'; m2'; m3';
-     0f0 0f0 0f0 1f0]
-end
-#intended usage index 1,2,3 = x,y,z
-immutable Vec{Cardinality, T}
-    v::Array{T, 1}
-    function Vec(v::Array{T,1})
-        Cardinality == length(v)
-        new(v)
-    end
+    m1 = Vector4{T}( x^2 * (1 - cos(a)) + cos(a)  ,  x*y * (1 - cos(a)) - z*sin(a),  x*z * (1 - cos(a)) + y*sin(a), 0)
+    m2 = Vector4{T}( x*y * (1 - cos(a)) + z*sin(a),  y^2 * (1 - cos(a)) + cos(a)  ,  y*z * (1 - cos(a)) - x*sin(a), 0)
+    m3 = Vector4{T}( x*z * (1 - cos(a)) - y*sin(a),  y*z * (1 - cos(a)) + x*sin(a),  z^2 * (1 - cos(a)) + cos(a)  , 0)
+    m4 = Vector4{T}(0, 0, 0, 1)
+    Matrix4x4(m1, m2, m3, m4)
 end
 
-#typealias Vec32{Cardinality} Vec{Cardinality, Float32}
-Vec{T}(v::Array{T, 1}) = Vec{length(v), T}(v)
-Vec(v::Real...) = Vec([v...])
-
-
-Vec32{T <: Real}(v::Array{T,1}) = Vec{length(v), Float32}(float32(v))
-Vec32(v::Real...) = Vec32([v...])
 
 
 export lookAt, computeFOVProjection, computeFOVProjection!, computeOrthographicProjection, computeOrthographicProjection!, translateXMatrix, translationMatrix, pProj
