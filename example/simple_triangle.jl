843b1290df44128921935acb762951dd2c9e315a
Simon Danisch <sdanisch@gmail.com>
2014-07-26 17:17:02 +0200

    incorporated api changes

--- example/simple_triangle.jl
+++ example/simple_triangle.jl
@@ -1,7 +1,7 @@
-using ModernGL, GLWindow, GLAbstraction
+using ModernGL, GLWindow, GLAbstraction, GLFW
 
 
-const window = createWindow("Example", 512, 512)
+const window = createwindow("Example", 512, 512)
 
 
 const vsh = """
@@ -9,7 +9,7 @@ const vsh = """
 in vec2 position;
  
 void main() {
-gl_Position = vec4(position, 0.0, 1.0);
+	gl_Position = vec4(position, 0.0, 1.0);
 }
 """
  
@@ -18,26 +18,24 @@ const fsh = """
 out vec4 outColor;
  
 void main() {
-outColor = vec4(1.0, 1.0, 1.0, 1.0);
+	outColor = vec4(1.0, 1.0, 1.0, 1.0);
 }
 """
 
 const triangle = RenderObject(
-	[:position => GLBuffer(GLfloat[0.0, 0.5, 0.5, -0.5, -0.5,-0.5], 2),], 
-	GLProgram(vsh, fsh, "simpleshader"))
+	[:position => GLBuffer(GLfloat[0.0, 0.5, 0.5, -0.5, -0.5,-0.5], 2)], 
+	GLProgram(vsh, fsh, "vert", "frag"))
+postrender!(triangle, render, triangle.vertexarray)
+glClearColor(0, 0, 0, 1)
 
 
-function test(x::RenderObject)
-	glClearColor(0.0, 0.0,0.0, 1.0)
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+while !GLFW.WindowShouldClose(window.glfwWindow)
 
-	programID = x.vertexArray.program.id
-	glUseProgram(programID)
-	render(x.uniforms)
-	glBindVertexArray(x.vertexArray.id)
-	glDrawArrays(GL_TRIANGLES, 0, x.vertexArray.length)
+  	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+	render(triangle)
+  	
+  	GLFW.SwapBuffers(window.glfwWindow)
+  	GLFW.PollEvents()
 end
-
-glDisplay(:triangle, test, triangle)
-
-renderloop(window)
+GLFW.Terminate()
