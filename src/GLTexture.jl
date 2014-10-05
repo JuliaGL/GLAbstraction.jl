086486558540be2b9420a38695e179f57e479bf8
SimonDanisch <sdanisch@gmail.com>
2014-10-05 19:04:48 +0200

    added one dimensional array acces to 2d textures

--- src/GLTexture.jl
+++ src/GLTexture.jl
@@ -1,7 +1,7 @@
 export Texture, texturetype, update!, data
 
 SupportedEltypes = Union(Real, AbstractArray, ColorValue, AbstractAlphaColorValue)
-begin 
+begin
     #Supported datatypes
     local const TO_GL_TYPE = [
         GLubyte     => GL_UNSIGNED_BYTE,
@@ -20,7 +20,7 @@ begin
     glpixelformat(x::SupportedEltypes)               = glpixelformat(eltype(x))
 end
 #Supported texture modes/dimensions
-begin 
+begin
     local const TO_GL_TEXTURE_TYPE = [
         1 => GL_TEXTURE_1D,
         2 => GL_TEXTURE_2D,
@@ -56,7 +56,7 @@ immutable Texture{T <: Union(SupportedEltypes, Real), ColorDIM, NDIM}
         end
 
         pixeltype = glpixelformat(T)
-        
+
         glTexImage(ttype, 0, internalformat, dims..., 0, format, pixeltype, data)
         new(id, ttype, pixeltype, internalformat, format, [dims...])
     end
@@ -77,7 +77,7 @@ end
 
 default_colorformat{T <: Real}(colordim::Type{T})          = default_colorformat(1, T <: Integer, "RED")
 default_colorformat{T <: AbstractArray}(colordim::Type{T}) = default_colorformat(length(T), eltype(T) <: Integer, "RGBA")
-function default_colorformat{T <: AbstractAlphaColorValue}(colordim::Type{T}) 
+function default_colorformat{T <: AbstractAlphaColorValue}(colordim::Type{T})
     colororder = string(T.parameters[1].name) * "A"
     return default_colorformat(length(T), eltype(T) <: Integer, colororder)
 end
@@ -97,7 +97,7 @@ function default_internalcolorformat(colordim::Int, typ::DataType)
     elseif eltyp <: FixedPoint
         sym *= eltyp <: Ufixed ? "" : "_SNORM"
     elseif eltyp <: Signed
-        sym *= "I"   
+        sym *= "I"
     elseif eltyp <: Unsigned
         sym *= "UI"
     end
@@ -120,7 +120,7 @@ function default_textureparameters(dim::Int, typ::DataType)
     parameters
 end
 
-# As the default parameters of a texture are dependent on the texture, this is done by a function. 
+# As the default parameters of a texture are dependent on the texture, this is done by a function.
 # The function overwrites the defaults with values from texture_properties, so that one can customize the defaults
 # Here is a good place for parameter checking, yet not implemented though...
 function gendefaults(texture_properties::Vector{(Symbol, Any)}, ColorDim::Integer, Typ::DataType, NDim::Integer)
@@ -134,25 +134,25 @@ end
 
 #=
 As Texture has a lot of variations with the same Keyword arguments, I decided to
-map the keywords into one array, which I pass to the actual constructor 
+map the keywords into one array, which I pass to the actual constructor
 =#
 Texture(data... ; texture_properties...) = Texture(data..., convert(Vector{(Symbol, Any)}, texture_properties))
 
 #=
-Main constructor, which shouldn't be used. It will initializes all the missing values and pass it to the inner Texture constructor 
+Main constructor, which shouldn't be used. It will initializes all the missing values and pass it to the inner Texture constructor
 =#
 function Texture{T <: SupportedEltypes}(data::Ptr{T}, dims::AbstractVector, texture_properties::Vector{(Symbol, Any)})
     Base.length{ET <: Real}(::Type{ET}) = 1
     NDim            = length(dims)
     ColorDim        = length(T)
     defaults        = gendefaults(texture_properties, ColorDim, T, NDim)
-    
+
     Texture{T, ColorDim, NDim}(data, dims, defaults[:texturetype], defaults[:internalformat], defaults[:format], defaults[:parameters])
 end
 
-#= 
+#=
 Constructor for empty initialization with NULL pointer instead of an array with data.
-You just need to pass the wanted color/vector type and the dimensions. 
+You just need to pass the wanted color/vector type and the dimensions.
 To which values the texture gets initialized is driver dependent
 =#
 function Texture{T <: SupportedEltypes}(datatype::Type{T}, dims::AbstractVector, texture_properties::Vector{(Symbol, Any)})
@@ -179,7 +179,7 @@ function Texture{T <: Real, NDim}(image::Array{ARGB{T}, NDim}, texture_propertie
     Texture(pointer(data), [size(data)...], texture_properties)
 end
 
-#= 
+#=
 Creates a texture from an image, which lays on path
 =#
 Texture(path::String, texture_properties::Vector{(Symbol, Any)}) = Texture(imread(path), texture_properties)
@@ -192,9 +192,9 @@ function Texture(image::Image, texture_properties::Vector{(Symbol, Any)})
 end
 
 
-width(t::Texture)   = size(t,1) 
-height(t::Texture)  = size(t,2) 
-depth(t::Texture)   = size(t,3) 
+width(t::Texture)   = size(t,1)
+height(t::Texture)  = size(t,2)
+depth(t::Texture)   = size(t,3)
 
 Base.length(t::Texture) = prod(t.dims)
 Base.size(t::Texture) = tuple(t.dims...)
@@ -232,6 +232,16 @@ end
 function Base.setindex!{T <: SupportedEltypes, ColorDim, IT1 <: Integer}(t::Texture{T, ColorDim, 1}, value, i::UnitRange{IT1})
     update!(t, value, first(i))
 end
+
+
+function Base.setindex!{T <: SupportedEltypes, ColorDim, IT1 <: Integer}(t::Texture{T, ColorDim, 2}, value, i::UnitRange{IT1})
+    a = mod1(first(i), size(t, 1))
+    b = div(first(i), size(t, 1))+1
+    setindex!(t, value, a, b)
+end
+function Base.setindex!{T <: SupportedEltypes, ColorDim}(t::Texture{T, ColorDim, 2}, value, i::Integer)
+    setindex!(t, value, mod1(i,size(t, 1)), div(i+1,size(t, 1))-1)
+end
 function Base.setindex!{T <: SupportedEltypes, ColorDim}(t::Texture{T, ColorDim, 2}, value, i::Integer, j::Integer)
     update!(t, value, i, j)
 end
@@ -257,7 +267,9 @@ function update!{T <: SupportedEltypes, ColorDim}(t::Texture{T, ColorDim, 1}, ne
     glTexSubImage1D(t.texturetype, 0, xoffset-1, _width, t.format, t.pixeltype, newvalue)
 end
 
-
+function update!{T <: SupportedEltypes, ColorDim}(t::Texture{T, ColorDim, 2}, newvalue::Array{T, 1}, xoffset = 0, yoffset = 0)
+    update!(t, newvalue, xoffset, yoffset, length(newvalue), 1)
+end
 function update!{T <: SupportedEltypes, ColorDim}(t::Texture{T, ColorDim, 2}, newvalue::Array{T, 2}, xoffset = 0, yoffset = 0)
     update!(t, newvalue, xoffset, yoffset, size(newvalue)...)
 end
@@ -267,7 +279,7 @@ function update!{T <: SupportedEltypes, ColorDim}(t::Texture{T, ColorDim, 2}, ne
 end
 
 
-function update!{T <: SupportedEltypes, ColorDim}(t::Texture{T, ColorDim, 2}, newvalue::Array, xoffset, yoffset, _width, _height) 
+function update!{T <: SupportedEltypes, ColorDim}(t::Texture{T, ColorDim, 2}, newvalue::Array, xoffset, yoffset, _width, _height)
     if (xoffset-1 + _width) > width(t) && (yoffset-1 + _height) > height(t)
         error("Out of bounds in texture, xindex ", xoffset, " width: ", _width, " yindex: ", yoffset, " height: ", _height, " in texture:\n", t)
     end
