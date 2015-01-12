7b7e34f73130caf362e962ca492cc9445124b9f3
Simon Danisch <sdanisch@gmail.com>
2015-01-12 23:44:51 +0100

    removed pointer warnings

--- src/GLUniforms.jl
+++ src/GLUniforms.jl
@@ -42,7 +42,7 @@ macro genuniformfunctions(maxdim::Integer)
 			push!(expressions, :(gluniform(location::GLint, x::$typ) = $uniformfunc(location, x)))
 		end
 		push!(expressions, :(typealias $glslalias $imalias)) # glsl alike type alias
-		push!(expressions, :(gluniform(location::Integer, x::$imalias) = $uniformfunc(location, 1, [x]))) # uniform function for single uniforms
+		push!(expressions, :(gluniform(location::Integer, x::$imalias) = $uniformfunc(location, 1, pointer([x])))) # uniform function for single uniforms
 		push!(expressions, :(gluniform(location::Integer, x::Vector{$imalias}) = $uniformfunc(location, length(x), pointer(x)))) #uniform function for arrays of uniforms
 		if n > 1
 			push!(expressions, :($glslalias(x::Real) = $name(convert($typ, x)))) # Single valued constructor
@@ -64,7 +64,7 @@ macro genuniformfunctions(maxdim::Integer)
 		uniformfunc = symbol(string("glUniformMatrix", glsldim, GL_POSTFIX[typ]))
 
 		push!(expressions, :(typealias $glslalias $imalias)) #GLSL alike alias
-		push!(expressions, :(gluniform(location::Integer, x::$imalias) = $uniformfunc(location, 1, GL_FALSE, [x]))) # uniform function for single uniforms
+		push!(expressions, :(gluniform(location::Integer, x::$imalias) = $uniformfunc(location, 1, GL_FALSE, pointer([x])))) # uniform function for single uniforms
 		push!(expressions, :(gluniform(location::Integer, x::Vector{$imalias}) = $uniformfunc(location, length(x), GL_FALSE, pointer(x)))) #uniform function for arrays of uniforms
 		push!(expressions, :($glslalias(x::Real) = $name(convert($typ, x)))) # Single valued constructor
 		push!(expressions, Expr(:export, glslalias))
@@ -111,15 +111,15 @@ gluniform(location::Integer, x::Union(GLbyte, GLshort, GLint, Bool)) = glUniform
 
 # Needs to be 
 gluniform(location::Integer, x::Vector{RGB{Float32}}) = glUniform3fv(location, length(x), pointer(x))
-gluniform(location::Integer, x::AbstractRGB{Float32}) = glUniform3fv(location, 1, [x])
-gluniform(location::Integer, x::AbstractAlphaColorValue{RGB{Float32}, Float32}) = glUniform4fv(location, 1, [x])
+gluniform(location::Integer, x::AbstractRGB{Float32}) = glUniform3fv(location, 1, pointer([x]))
+gluniform(location::Integer, x::AbstractAlphaColorValue{RGB{Float32}, Float32}) = glUniform4fv(location, 1, pointer([x]))
 gluniform(location::Integer, x::AbstractAlphaColorValue) = gluniform(location, convert(AbstractAlphaColorValue{RGB{Float32}, Float32}, x))
 
 
 #Uniform upload functions for julia arrays...
-gluniform(location::GLint, x::Vector{Float32}) 	= glUniform1fv(location, length(x), x)
-gluniform(location::GLint, x::Vector{GLint}) 	= glUniform1iv(location, length(x), x)
-gluniform(location::GLint, x::Vector{GLuint}) 	= glUniform1uiv(location, length(x), x)
+gluniform(location::GLint, x::Vector{Float32}) 	= glUniform1fv(location, length(x), pointer(x))
+gluniform(location::GLint, x::Vector{GLint}) 	= glUniform1iv(location, length(x), pointer(x))
+gluniform(location::GLint, x::Vector{GLuint}) 	= glUniform1uiv(location, length(x), pointer(x))
 
 
 glsl_prefix(x::DataType) = GLSL_PREFIX[x]
