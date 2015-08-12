4ba9c449f0a1b394b3b2376c0fef34a7b3ba03e5
Sdanisch <sdanisch@gmail.com>
2015-08-12 23:36:14 +0200

    cosmetic

--- src/GLCamera.jl
+++ src/GLCamera.jl
@@ -10,8 +10,8 @@ type PerspectiveCamera{T} <: Camera{T}
 	pivot 			::Signal{Pivot{T}}
 	window_size 	::Signal{Rectangle{Int}}
 	nearclip 		::Signal{T}
-  	farclip 		::Signal{T}
-  	fov 			::Signal{T}
+	farclip 		::Signal{T}
+	fov 			::Signal{T}
 	view 			::Signal{Mat{4,4,T}}
 	projection 		::Signal{Mat{4,4,T}}
 	projectionview 	::Signal{Mat{4,4,T}}
@@ -28,12 +28,12 @@ type DummyCamera{T} <: Camera{T}
 end
 
 function DummyCamera(;
-		window_size		= Input(Rectangle(-1, -1, 1, 1)),
-		view 			= Input(eye(Mat{4,4, Float32})),
-		nearclip 		= Input(10000f0),
-		farclip 		= Input(-10000f0),
-		projection 		= lift(orthographicprojection, window_size, nearclip, farclip),
-		projectionview 	= lift(*, projection, view)
+	window_size		= Input(Rectangle(-1, -1, 1, 1)),
+	view 			= Input(eye(Mat{4,4, Float32})),
+	nearclip 		= Input(10000f0),
+	farclip 		= Input(-10000f0),
+	projection 		= lift(orthographicprojection, window_size, nearclip, farclip),
+	projectionview 	= lift(*, projection, view)
 	)
 	DummyCamera{Float32}(window_size, view, projection, projectionview)
 end
@@ -48,9 +48,9 @@ function Base.collect(camera::Camera)
 end
 
 function mousediff{T}(v0::Tuple{Bool, Vec{2, T}, Vec{2, T}},  clicked::Bool, pos::Vec{2, T})
-    clicked0, pos0, pos0diff = v0
-    (clicked0 && clicked) && return (clicked, pos, pos - pos0)
-    return (clicked, pos, Vec{2, T}(0.0))
+	clicked0, pos0, pos0diff = v0
+	(clicked0 && clicked) && return (clicked, pos, pos - pos0)
+	return (clicked, pos, Vec{2, T}(0.0))
 end
 function viewmatrix(v0, scroll_x, scroll_y, buttonset)
 	translatevec = Vec3f0(0f0)
@@ -65,18 +65,18 @@ function viewmatrix(v0, scroll_x, scroll_y, buttonset)
 	else
 		translatevec = Vec3f0(scroll_x*10f0, scroll_y*10f0, 0f0)
 	end
-	v0 * translationmatrix(translatevec)	
+	v0 * translationmatrix(translatevec)
 end
 
-#= 
+#=
 Creates an orthographic camera with the pixel perfect plane in z == 0
 Signals needed:
 [
-	:window_size					=> Input(Rectangle{Int}),
-	:buttonspressed					=> Input(Int[]),
-	:mousebuttonspressed			=> Input(Int[]), 
-	:mouseposition					=> mouseposition, -> Panning
-	:scroll_y						=> Input(0) -> Zoomig
+:window_size					=> Input(Rectangle{Int}),
+:buttonspressed					=> Input(Int[]),
+:mousebuttonspressed			=> Input(Int[]),
+:mouseposition					=> mouseposition, -> Panning
+:scroll_y						=> Input(0) -> Zoomig
 ]
 =#
 function OrthographicPixelCamera(inputs::Dict{Symbol, Any})
@@ -84,10 +84,10 @@ function OrthographicPixelCamera(inputs::Dict{Symbol, Any})
 	#Should be rather in Image coordinates
 	view = foldl(viewmatrix, eye(Mat{4,4, Float32}), inputs[:scroll_x], inputs[:scroll_y], buttonspressed)
 	OrthographicCamera(
-		inputs[:window_size],
-		view,
-		Input(-10f0), # nearclip
-		Input(10f0) # farclip
+	inputs[:window_size],
+	view,
+	Input(-10f0), # nearclip
+	Input(10f0) # farclip
 	)
 
 end
@@ -96,56 +96,56 @@ end
 times_n(v0, v1, n) = Float32(v0+(v1*n))
 normalize_positionf0(mouse, window) = Vec2f0(mouse) ./ Vec2f0(window.w, window.h)
 is_leftclicked_without_keyboard(mb, kb) = in(0, mb) && isempty(kb)
-#= 
+#=
 Creates an orthographic camera from a dict of signals
 Signals needed:
 [
-	:window_size					=> Input(Vec{2, Int}),
-	:buttonspressed					=> Input(Int[]),
-	:mousebuttonspressed			=> Input(Int[]), 
-	:mouseposition					=> mouseposition, -> Panning
-	:scroll_y						=> Input(0) -> Zoomig
+:window_size					=> Input(Vec{2, Int}),
+:buttonspressed					=> Input(Int[]),
+:mousebuttonspressed			=> Input(Int[]),
+:mouseposition					=> mouseposition, -> Panning
+:scroll_y						=> Input(0) -> Zoomig
 ]
 =#
 function OrthographicCamera(inputs::Dict{Symbol, Any})
 	@materialize mouseposition, mousebuttonspressed, buttonspressed, window_size = inputs
-	
+
 	zoom 				= foldl(times_n , 1.0f0, inputs[:scroll_y], Input(0.1f0)) # add up and multiply by 0.1f0
 	#Should be rather in Image coordinates
 	normedposition 		= lift(normalize_positionf0, inputs[:mouseposition], inputs[:window_size])
 	clickedwithoutkeyL 	= lift(is_leftclicked_without_keyboard, mousebuttonspressed, buttonspressed)
 
-                         # Note 1: Don't do unnecessary updates, so just signal when mouse is actually clicked
-                         # Note 2: Get the difference, starting when the mouse is down
-	mouse_diff 			= keepwhen(clickedwithoutkeyL, (false, Vec2f0(0.0f0), Vec2f0(0.0f0)),  ## (Note 1) 
-					               foldl(mousediff, (false, Vec2f0(0.0f0), Vec2f0(0.0f0)), ## (Note 2) 
-									clickedwithoutkeyL, normedposition))
-    translate 			= lift(getindex, mouse_diff, Input(3))  # Extract the mouseposition from the diff tuple
-    
+	# Note 1: Don't do unnecessary updates, so just signal when mouse is actually clicked
+	# Note 2: Get the difference, starting when the mouse is down
+	mouse_diff 			= keepwhen(clickedwithoutkeyL, (false, Vec2f0(0.0f0), Vec2f0(0.0f0)),  ## (Note 1)
+	foldl(mousediff, (false, Vec2f0(0.0f0), Vec2f0(0.0f0)), ## (Note 2)
+	clickedwithoutkeyL, normedposition))
+	translate 			= lift(getindex, mouse_diff, Input(3))  # Extract the mouseposition from the diff tuple
+
 	OrthographicCamera(
-		window_size,
-		zoom,
-		translate,
-		normedposition
+	window_size,
+	zoom,
+	translate,
+	normedposition
 	)
 end
-#= 
+#=
 Creates an orthographic camera from signals, controlling the camera
 Args:
 
-   window_size: Size of the window
-   		  zoom: Zoom
-  translatevec: Panning
+window_size: Size of the window
+zoom: Zoom
+translatevec: Panning
 normedposition: Pivot for translations
 =#
 function OrthographicCamera{T}(
-		windows_size::Signal{Rectangle{Int}},
-		view 		::Signal{Mat{4,4,T}},
-		nearclip 	::Signal{T},
-		farclip 	::Signal{T}
+	windows_size::Signal{Rectangle{Int}},
+	view 		::Signal{Mat{4,4,T}},
+	nearclip 	::Signal{T},
+	farclip 	::Signal{T}
 	)
 
-	projection = lift(orthographicprojection, windows_size, nearclip, farclip) 
+	projection = lift(orthographicprojection, windows_size, nearclip, farclip)
 	#projection = Input(eye(Mat4))
 	#view = Input(eye(Mat4))
 	projectionview = lift(*, projection, view)
@@ -160,21 +160,21 @@ end
 
 
 
-#= 
+#=
 Creates an orthographic camera from signals, controlling the camera
 Args:
 
-   window_size: Size of the window
-   		  zoom: Zoom
-  translatevec: Panning
+window_size: Size of the window
+zoom: Zoom
+translatevec: Panning
 normedposition: Pivot for translations
 
 =#
 function OrthographicCamera{T}(
-		windows_size 	::Signal{Rectangle{Int}},
-		zoom 			::Signal{T},
-		translatevec 	::Signal{Vec{2, T}},
-		normedposition 	::Signal{Vec{2, T}}
+	windows_size 	::Signal{Rectangle{Int}},
+	zoom 			::Signal{T},
+	translatevec 	::Signal{Vec{2, T}},
+	normedposition 	::Signal{Vec{2, T}}
 	)
 
 	projection = lift(windows_size) do wh
@@ -193,38 +193,38 @@ function OrthographicCamera{T}(
 	translate         = lift(x-> translationmatrix(Vec{3,T}(x, zero(T))), transaccum)
 
 	view = lift((s, t) -> begin
-	  pivot = Vec(normedposition.value..., zero(T))
-	  translationmatrix(pivot)*s*translationmatrix(-pivot)*t
-	end, scale, translate)
+	pivot = Vec(normedposition.value..., zero(T))
+	translationmatrix(pivot)*s*translationmatrix(-pivot)*t
+end, scale, translate)
 
-	projectionview = lift(*, projection, view)
+projectionview = lift(*, projection, view)
 
-	OrthographicCamera{T}(
-		windows_size,
-		projection,
-		view,
-		projectionview
-	)
+OrthographicCamera{T}(
+windows_size,
+projection,
+view,
+projectionview
+)
 
 end
 
-mousepressed_without_keyboard(mousebuttons::Vector{Int}, button::Int, keyboard::Vector{Int}) = 
-	in(button, mousebuttons) && isempty(keyboard)
+mousepressed_without_keyboard(mousebuttons::Vector{Int}, button::Int, keyboard::Vector{Int}) =
+in(button, mousebuttons) && isempty(keyboard)
 
-#= 
+#=
 Creates a perspective camera from a dict of signals
 Args:
 
-      inputs: Dict of signals, looking like this:
-			[
-				:window_size					=> Input(Vec{2, Int}),
-				:buttonspressed					=> Input(Int[]),
-				:mousebuttonspressed			=> Input(Int[]), 
-				:mouseposition					=> mouseposition, -> Panning + Rotation
-				:scroll_y						=> Input(0) -> Zoomig
-			]
-  eyeposition: Position of the camera
-	lookatvec: Point the camera looks at
+inputs: Dict of signals, looking like this:
+[
+:window_size					=> Input(Vec{2, Int}),
+:buttonspressed					=> Input(Int[]),
+:mousebuttonspressed			=> Input(Int[]),
+:mouseposition					=> mouseposition, -> Panning + Rotation
+:scroll_y						=> Input(0) -> Zoomig
+]
+eyeposition: Position of the camera
+lookatvec: Point the camera looks at
 =#
 function PerspectiveCamera{T}(inputs::Dict{Symbol,Any}, eyeposition::Vec{3, T}, lookatvec::Vec{3, T})
 	@materialize mouseposition, mousebuttonspressed, buttonspressed, scroll_y, window_size = inputs
@@ -248,20 +248,20 @@ function PerspectiveCamera{T}(inputs::Dict{Symbol,Any}, eyeposition::Vec{3, T}, 
 	ztrans = lift(/, lift(last, mousedraggdiffM), speed) # (mouse.x / speed)
 
 	cam = PerspectiveCamera(
-		window_size,
-		eyeposition,
-		lookatvec,
-		
-		xtheta,
-		ytheta,
-		ztheta,
+	window_size,
+	eyeposition,
+	lookatvec,
 
-		xtrans,
-		ytrans,
-		ztrans,
-		Input(41f0),
-		Input(1f0),
-		Input(100f0)
+	xtheta,
+	ytheta,
+	ztheta,
+
+	xtrans,
+	ytrans,
+	ztrans,
+	Input(41f0),
+	Input(1f0),
+	Input(100f0)
 	)
 end
 
@@ -279,55 +279,55 @@ function fold_pivot(v0, v1)
 
 	v1rot 	= zrot*xrot*yrot*v0.rotation
 
-	v1trans    = yaxis*ytr + zaxis*ztr 
+	v1trans    = yaxis*ytr + zaxis*ztr
 	accumtrans = v1trans + v0.translation
 
 	Pivot(
-		v0.origin + v1trans, 
-		v0.xaxis, 
-		v0.yaxis, 
-		v0.zaxis, 
-		v1rot, 
-		accumtrans + v0.xaxis*xtr, 
-		v0.scale
+	v0.origin + v1trans,
+	v0.xaxis,
+	v0.yaxis,
+	v0.zaxis,
+	v1rot,
+	accumtrans + v0.xaxis*xtr,
+	v0.scale
 	)
 end
 
 getupvec(p::Pivot) = p.rotation * p.zaxis
-#= 
+#=
 Creates a perspective camera from signals, controlling the camera
 Args:
 
-   window_size: Size of the window
-   		  zoom: Zoom
-   eyeposition: Position of the camera
-     lookatvec: Point the camera looks at
-     	
-     	xtheta: xrotation angle
-     	ytheta: yrotation angle
-     	ztheta: zrotation angle
-     	
-     	xtrans: x translation
-     	ytrans: y translation
-     	ztrans: z translation
-		   fov: Field of View
-	  nearclip: Near clip plane
-	   farclip: Far clip plane
+window_size: Size of the window
+zoom: Zoom
+eyeposition: Position of the camera
+lookatvec: Point the camera looks at
+
+xtheta: xrotation angle
+ytheta: yrotation angle
+ztheta: zrotation angle
+
+xtrans: x translation
+ytrans: y translation
+ztrans: z translation
+fov: Field of View
+nearclip: Near clip plane
+farclip: Far clip plane
 
 =#
 function PerspectiveCamera{T <: Real}(
-					window_size 	::Signal{Rectangle{Int}},
-					eyeposition 	::Vec{3, T},
-					lookatvec 		::Vec{3, T},
-					xtheta 			::Signal{T},
-					ytheta 			::Signal{T},
-					ztheta 			::Signal{T},
-					xtrans 			::Signal{T},
-					ytrans 			::Signal{T},
-					ztrans 			::Signal{T},
-					fov 			::Signal{T},
-					nearclip 		::Signal{T},
-					farclip 		::Signal{T}
+		window_size 	::Signal{Rectangle{Int}},
+		eyeposition 	::Vec{3, T},
+		lookatvec 		::Vec{3, T},
+		xtheta 			::Signal{T},
+		ytheta 			::Signal{T},
+		ztheta 			::Signal{T},
+		xtrans 			::Signal{T},
+		ytrans 			::Signal{T},
+		ztrans 			::Signal{T},
+		fov 			::Signal{T},
+		nearclip 		::Signal{T},
+		farclip 		::Signal{T}
 	)
 
 	eyepositionstart= Vec{3, T}(1,0,0)
@@ -359,18 +359,16 @@ function PerspectiveCamera{T <: Real}(
 
 
 	return PerspectiveCamera{T}(
-		pivot,
-		window_size,
-		nearclip,
-		farclip,
-		fov,
-		view,
-		projection,
-		projectionview,
-		positionvec,
-		lookatvec1,
-		up
+	pivot,
+	window_size,
+	nearclip,
+	farclip,
+	fov,
+	view,
+	projection,
+	projectionview,
+	positionvec,
+	lookatvec1,
+	up
 	)
 end
-
-
