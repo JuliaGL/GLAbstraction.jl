be8546226ba61ef96ec02e270126f900e3a77492
Sdanisch <sdanisch@gmail.com>
2015-10-01 19:01:36 +0200

    better test for CI

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,9 @@
 using GLAbstraction, GeometryTypes, ModernGL, Compat, FileIO, GLFW
-if isinteractive() # only do test if called from REPL... this is for automated testing environments which fail for OpenGL stuff, but I'd like to test if at least including works
+function is_ci()
+	get(ENV, "TRAVIS", "") == "true" || get(ENV, "APPVEYOR", "") == "true" || get(ENV, "CI", "") == "true"
+end
+
+if !is_ci() # only do test if not CI... this is for automated testing environments which fail for OpenGL stuff, but I'd like to test if at least including works
 
 # initilization,  with GLWindow this reduces to "createwindow("name", w,h)"
 GLFW.Init()
