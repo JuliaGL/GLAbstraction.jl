0de6436e3dc21f9ce37f2acb7e56cfb37986b593
SimonDanisch <sdanisch@gmail.com>
2015-12-03 22:30:53 +0100

    fixed enum uniform passing

--- src/GLUniforms.jl
+++ src/GLUniforms.jl
@@ -59,11 +59,11 @@ function gluniform(location::GLint, target::GLint, t::Texture)
     glBindTexture(t.texturetype, t.id)
     gluniform(location, target)
 end
-gluniform(location::Integer, x::Signal)                              = gluniform(GLint(location),    value(x))
+gluniform(location::Integer, x::Enum) 	 						     = gluniform(GLint(location), GLint(x))
+gluniform(location::Integer, x::Signal)                              = gluniform(GLint(location), value(x))
 gluniform(location::Integer, x::Union{GLubyte, GLushort, GLuint}) 	 = glUniform1ui(GLint(location), x)
 gluniform(location::Integer, x::Union{GLbyte, GLshort, GLint, Bool}) = glUniform1i(GLint(location),  x)
 gluniform(location::Integer, x::GLfloat)                             = glUniform1f(GLint(location),  x)
-gluniform(location::Integer, x::Enum) 	 						     = glUniform1f(GLint(location),  GLint(x))
 
 
 #Uniform upload functions for julia arrays...
@@ -79,7 +79,7 @@ end
 function toglsltype_string{T}(t::TextureBuffer{T})
     string("uniform ", opengl_prefix(eltype(T)),"samplerBuffer")
 end
-toglsltype_string(t::Nothing)                   = "uniform Nothing"
+toglsltype_string(t::Nothing)                   = "Nothing"
 toglsltype_string(t::GLfloat)                   = "uniform float"
 toglsltype_string(t::GLuint)                    = "uniform uint"
 toglsltype_string(t::GLint)                     = "uniform int"
