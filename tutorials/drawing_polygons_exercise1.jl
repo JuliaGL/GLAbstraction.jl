6852c9271ec085e03d7baba8e73769ee571b8bd0
Tim Holy <tim.holy@gmail.com>
2016-02-18 06:26:15 -0600

    Add answers to exercises

---
+++ tutorials/drawing_polygons_exercise1.jl
@@ -0,0 +1,62 @@
+# Here, we illustrate a more "julian" implementation that leverages
+# some of the advantages of GLAbstraction
+import GLFW
+using ModernGL, GeometryTypes, GLAbstraction
+
+# Create the window
+window = GLFW.CreateWindow(800, 600, "Drawing polygons 5")
+GLFW.MakeContextCurrent(window)
+# Retain keypress events
+GLFW.SetInputMode(window, GLFW.STICKY_KEYS, GL_TRUE)
+
+# A slightly-simplified VAO generator call
+vao = glGenVertexArrays()
+glBindVertexArray(vao)
+
+# The positions of the vertices in our rectangle
+vertex_positions = Point{2,Float32}[(-0.5,  0.5),     # top-left
+                                    ( 0.5,  0.5),     # top-right
+                                    ( 0.0, -0.5)]     # bottom-center
+
+# The vertex shader---note the `vert` in front of """
+vertex_shader = vert"""
+#version 150
+
+in vec2 position;
+
+void main()
+{
+    gl_Position = vec4(position, 0.0, 1.0);
+}
+"""
+
+# The fragment shader
+fragment_shader = frag"""
+# version 150
+
+out vec4 outColor;
+
+void main()
+{
+    outColor = vec4(1.0, 1.0, 1.0, 1.0);
+}
+"""
+
+# Link everything together, using the corresponding shader variable as
+# the Dict key
+bufferdict = Dict(:position=>GLBuffer(vertex_positions))
+
+ro = std_renderobject(bufferdict,
+                      LazyShader(vertex_shader, fragment_shader))
+
+# Draw until we receive a close event
+while !GLFW.WindowShouldClose(window)
+    render(ro)
+    GLFW.SwapBuffers(window)
+    GLFW.PollEvents()
+    if GLFW.GetKey(window, GLFW.KEY_ESCAPE) == GLFW.PRESS
+        GLFW.SetWindowShouldClose(window, true)
+    end
+end
+
+GLFW.DestroyWindow(window)  # needed if you're running this from the REPL
