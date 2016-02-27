157f2b5c5694d526ad0bf650c871554d69b9e19a
SimonDanisch <sdanisch@gmail.com>
2016-02-27 13:55:29 +0100

    fixes window initialization

--- tutorials/textures_exercise1.jl
+++ tutorials/textures_exercise1.jl
@@ -2,16 +2,37 @@ import GLFW
 using ModernGL, GeometryTypes, GLAbstraction, Images
 
 # Load our textures. See "downloads.jl" to get the images.
-kitten = load("images/kitten.png")
-puppy  = load("images/puppy.png")
+kitten = load(Pkg.dir("GLAbstraction", "tutorials", "kitten.png"))
+puppy  = load(Pkg.dir("GLAbstraction", "tutorials", "puppy.png"))
 
 # Create the window
+# Specify minimum versions
+# It's always good to set all window hints, since it reduces platform variances!
+window_hint = [
+    (GLFW.SAMPLES,      4),
+    (GLFW.DEPTH_BITS,   0),
+
+    (GLFW.ALPHA_BITS,   8),
+    (GLFW.RED_BITS,     8),
+    (GLFW.GREEN_BITS,   8),
+    (GLFW.BLUE_BITS,    8),
+    (GLFW.STENCIL_BITS, 0),
+    (GLFW.AUX_BUFFERS,  0),
+    (GLFW.CONTEXT_VERSION_MAJOR, 3),# minimum OpenGL v. 3
+    (GLFW.CONTEXT_VERSION_MINOR, 0),# minimum OpenGL v. 3.2
+    (GLFW.OPENGL_PROFILE, GLFW.OPENGL_ANY_PROFILE),
+    (GLFW.OPENGL_FORWARD_COMPAT, GL_TRUE),
+
+]
+
+for (key, value) in window_hint
+    GLFW.WindowHint(key, value)
+end
+
 window = GLFW.CreateWindow(800, 800, "Textures 2")
 GLFW.MakeContextCurrent(window)
+GLFW.ShowWindow(window)
 GLFW.SetInputMode(window, GLFW.STICKY_KEYS, GL_TRUE)
-
-vao = glGenVertexArrays()
-glBindVertexArray(vao)
 
 # The positions of the vertices in our rectangle
 vertex_positions = Point{2,Float32}[(-0.5,  0.5),     # top-left
@@ -36,7 +57,7 @@ elements = Face{3,UInt32,-1}[(0,1,2),          # the first triangle
 
 # The vertex shader---note the `vert` in front of """
 vertex_shader = vert"""
-#version 150
+# version 150
 
 in vec2 position;
 in vec2 texcoord;
@@ -54,7 +75,6 @@ void main()
 fragment_shader = frag"""
 # version 150
 
-in vec3 Color;
 in vec2 Texcoord;
 
 out vec4 outColor;
@@ -73,22 +93,29 @@ void main()
 
 # Link everything together, using the corresponding shader variable as
 # the Dict key
-bufferdict = Dict(:position=>GLBuffer(vertex_positions),
-                  :texcoord=>GLBuffer(vertex_texcoords),
-                  :texKitten=>Texture(data(kitten)),
-                  :texPuppy=>Texture(data(puppy)),
-                  :indexes=>indexbuffer(elements)) # special for element buffers
+bufferdict = Dict(
+    :position=>GLBuffer(vertex_positions),
+    :texcoord=>GLBuffer(vertex_texcoords),
+    :texKitten=>Texture(data(kitten)),
+    :texPuppy=>Texture(data(puppy)),
+    :indexes=>indexbuffer(elements),
+    :blend_f=>1f0
+) # special for element buffers
 
-ro = std_renderobject(bufferdict,
-                      LazyShader(vertex_shader, fragment_shader))
-
-prog = ro.vertexarray.program
-blend_f = glGetUniformLocation(prog.id, "blend_f")
+ro = std_renderobject(
+    bufferdict,
+    LazyShader(
+    vertex_shader, fragment_shader,
+    fragdatalocation=[(0, "outColor")]
+))
 
 # Draw until we receive a close event
 period = 4
+glClearColor(0.0, 0.0, 0.0, 1.0)
+
 while !GLFW.WindowShouldClose(window)
-    glUniform1f(blend_f, Float32(mod(time(), period)/period))
+    glClear(GL_COLOR_BUFFER_BIT)
+    ro[:blend_f] = Float32(mod(time(), period)/period)
     render(ro)
     GLFW.SwapBuffers(window)
     GLFW.PollEvents()
