8a7eed9476cba4eaef5b6091626854f4227865f0
SimonDanisch <sdanisch@gmail.com>
2016-06-13 10:27:43 +0200

    fix up layout! and transform!

--- src/composition.jl
+++ src/composition.jl
@@ -9,31 +9,55 @@ type Context{Unit} <: Composable{Unit}
     boundingbox
     transformation
 end
-
-function translationmatrix(b)
-    const_lift(b) do b
-        m = minimum(b)
-        w = widths(b)
-        T = eltype(w)
-        # make code work also for N == 2
-        w3 = ndims(w) > 2 ?  w[3] : one(T)
-        m3 = ndims(m) > 2 ? m[3] : zero(T)
-
-        Mat4f0( # always return float32 matrix
-            (w[1], 0   , 0 , 0),
-            (0   , w[2], 0 , 0),
-            (0   , 0   , w3, 0),
-            (m[1], m[2], m3, 1),
-        )
-    end
+function scale_trans(b)
+    m = minimum(b)
+    w = widths(b)
+    T = eltype(w)
+    # make code work also for N == 2
+    w3 = ndims(w) > 2 ?  w[3] : one(T)
+    m3 = ndims(m) > 2 ? m[3] : zero(T)
+    Vec3f0(w[1], w[2], w3), Vec3f0(m[1], m[2], m3)
 end
-
+function translationmatrix(b)
+    s,t = scale_trans(b)
+    Mat4f0( # always return float32 matrix
+        (s[1], 0   , 0 , 0),
+        (0   , s[2], 0 , 0),
+        (0   , 0   , s[3], 0),
+        (t[1], t[2], t[3], 1),
+    )
+end
+function inversetransformation(b)
+    s,t = scale_trans(b)
+    s = 1f0/s
+    t = -t
+    Mat4f0( # always return float32 matrix
+        (s[1], 0   , 0 , 0),
+        (0   , s[2], 0 , 0),
+        (0   , 0   , s[3], 0),
+        (t[1], t[2], t[3], 1),
+    )
+end
 layout!(b, c) =  layout!(b, (c,))[1]
 layout!(b, c, composable...) = layout!(b, (c,composable...))
+function combine_s_t(scale_trans1, scale_trans2)
+    s1,t1 = scale_trans1
+    s2,t2 = scale_trans2
+    s2 = 1f0/s2
+    t2 = -t2
+    s,t = s1.*s2, t1+t2
+    Mat4f0( # always return float32 matrix
+        (s[1], 0   , 0 , 0),
+        (0   , s[2], 0 , 0),
+        (0   , 0   , s[3], 0),
+        (t[1], t[2], t[3], 1),
+    )
+end
 function layout!(b, composables::Union{Tuple, Vector})
-    trans = translationmatrix(b)
+    st_target = const_lift(scale_trans, b)
     map(composables) do composable
-        transform!(composable, trans)
+        st_being = const_lift(scale_trans, boundingbox(composable))
+        transform!(composable, map(combine_s_t, st_target, st_being))
         composable
     end
 end
