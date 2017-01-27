ed6631b38c88c07f90dcbf6d6c8c6b6176912eb5
Simon Danisch <sdanisch@gmail.com>
2017-01-27 16:28:56 -0500

    use style more consistently

--- example/compute_shader.jl
+++ example/compute_shader.jl
@@ -1,6 +1,6 @@
 using GLWindow, GLAbstraction, ModernGL, Reactive, GLFW, GeometryTypes
 
-const window = create_glcontext("Compute Shader", resolution=(512, 512))
+const window = create_glcontext("Compute Shader", resolution = (512, 512))
 
 # In order to write to a texture, we have to introduce it as image2D.
 # local_size_x/y/z layout variables define the work group size.
@@ -60,7 +60,7 @@ const tex = Texture(Float32, (512, 512))
 glBindImageTexture(0, tex.id, 0, GL_FALSE, 0, GL_WRITE_ONLY, tex.internalformat);
 
 data = Dict(
-    :roll    => roll,
+    :roll => roll,
     :destTex => tex,
 )
 const ro = RenderObject(data, Signal(prg))
@@ -92,7 +92,7 @@ data = merge(Dict(
 ), collect_for_gl(msh))
 
 textureshader = TemplateProgram(tex_frag, tex_vert, attributes=data)
-texobj           = RenderObject(data, Signal(textureshader))
+texobj = RenderObject(data, Signal(textureshader))
 
 prerender!(texobj, glDisable, GL_DEPTH_TEST, enabletransparency, glDisable, GL_CULL_FACE)
 postrender!(texobj, render, texobj.vertexarray)
