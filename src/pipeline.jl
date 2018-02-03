20dacca5e5d815da59f91d35da20d6ce6de0aa2c
Louis Ponet <louisponet@gmail.com>
2018-02-03 21:49:44 +0100

    made the pipeline a bit more flexible

--- src/pipeline.jl
+++ src/pipeline.jl
@@ -1,14 +1,14 @@
 #the idea of a pipeline together with renderpasses is that one can jus throw in a scene
 #and the render functions take care of what renderables get drawn by what passes
-struct Pipeline
+struct Pipeline{Name}
     name::Symbol
     passes::Vector{RenderPass}
     context::AbstractContext
 end
-
-Pipeline(name::Symbol, rps::Vector{RenderPass}) = Pipeline(name, rps, current_context())
+Pipeline(name::Symbol, rps::Vector{<:RenderPass}, context=current_context()) = Pipeline{name}(name, rps, context)
 
 function render(pipe::Pipeline, args...)
+    start(pipe)
     for pass in pipe.passes
         start(pass)
         pass(args...)
@@ -25,3 +25,5 @@ function free!(pipe::Pipeline)
     end
     return
 end
+#overload!
+start(pipe::Pipeline) = return
